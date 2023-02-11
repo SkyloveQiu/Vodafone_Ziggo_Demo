ec5c01495902e39a5bd84991ab0fb49e1d553eeb
Skylove Qiu <z.qiu@student.tudelft.nl>
2023-02-11 23:24:12 +0100

    add files

--- src/main/java/nl/vodafoneziggo/vodafoneziggo_demo/VodafoneZiggoDemoApplication.java
+++ src/main/java/nl/vodafoneziggo/vodafoneziggo_demo/VodafoneZiggoDemoApplication.java
@@ -1,8 +1,13 @@
 package nl.vodafoneziggo.vodafoneziggo_demo;
 
+
+import org.springdoc.core.configuration.SpringDocConfiguration;
+import org.springdoc.core.properties.SpringDocConfigProperties;
+import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -13,5 +18,20 @@ public class VodafoneZiggoDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(VodafoneZiggoDemoApplication.class, args);
     }
+
+    @Bean
+    SpringDocConfiguration springDocConfiguration(){
+        return new SpringDocConfiguration();
+    }
+    @Bean
+    SpringDocConfigProperties springDocConfigProperties() {
+        return new SpringDocConfigProperties();
+    }
+
+    @Bean
+    ObjectMapperProvider objectMapperProvider(SpringDocConfigProperties springDocConfigProperties){
+        return new ObjectMapperProvider(springDocConfigProperties);
+    }
+
 
 }
